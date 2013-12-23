2958015d2f7625d8d5d471ba261f84791bbacec1
Moky <albert.moky@gmail.com>
2013-12-24 00:41:54 +0800

    add comment.js

---
+++ js/comment.js
@@ -0,0 +1,15 @@
+;
+/**
+ *  Author:
+ *      Moky @ Dec. 23, 2013
+ */
+
+var duoshuoQuery = {short_name:"moky"};
+(function() {
+ var ds = document.createElement('script');
+ ds.type = 'text/javascript';ds.async = true;
+ ds.src = 'http://static.duoshuo.com/embed.js';
+ ds.charset = 'UTF-8';
+ (document.getElementsByTagName('head')[0]
+  || document.getElementsByTagName('body')[0]).appendChild(ds);
+ })();
