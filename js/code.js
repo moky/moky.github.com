9e283e7e365115f35d9867dc18a0992b26156324
Moky <albert.moky@gmail.com>
2013-12-24 16:07:52 +0800

    highlight codes

--- js/code.js
+++ js/code.js
@@ -4,33 +4,27 @@
  *      Moky @ Dec. 19, 2013
  */
 
-if (typeof(code) == "undefined") {
-	code = {
-	};
-}
-
-code.highlight = function() {
+function highlight() {
 	
-	function one() {
-		try {
-			var thiz = $(this);
-			var language = thiz.attr("language") || "javascript";
-			var html = thiz.html();
-			html = html.replace(/\</g, "&lt;");
-			html = html.replace(/\>/g, "&gt;");
-			var pre = $("<pre>" + html + "</pre>");
-			thiz.html("");
-			pre.appendTo(thiz);
-			pre.snippet(language, { style: "dull", showNum: true });
-		} catch(e) {
-			//alert("[code.js] error: " + e);
-		}
+	try {
+		var thiz = $(this);
+		var language = thiz.attr("language") || "javascript";
+		var html = thiz.html();
+		html = html.replace(/\</g, "&lt;");
+		html = html.replace(/\>/g, "&gt;");
+		var pre = $("<pre>" + html + "</pre>");
+		thiz.html("");
+		pre.appendTo(thiz);
+		pre.snippet(language, { style: "dull", showNum: true });
+	} catch(e) {
+		//alert("[code.js] error: " + e);
 	}
 	
-	$("code").each(one);
-};
+}
 
-tarsier.ready(code.highlight);
+tarsier.ready(function() {
+	$("code").each(highlight);
+});
 
 //tarsier.importCSS("http://steamdev.com/snippet/css/jquery.snippet.min.css");
 //tarsier.importJS("http://steamdev.com/snippet/js/jquery.snippet.min.js");
