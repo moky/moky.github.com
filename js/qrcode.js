b638631e10965a93bd27d1aff4430fedec428f9b
Moky <albert.moky@gmail.com>
2014-01-14 21:34:41 +0800

    qrcode
    tarsier.String

--- js/qrcode.js
+++ js/qrcode.js
@@ -4,33 +4,15 @@
  *      Moky @ Jan. 10, 2014
  */
 
-function utf16to8(str) {
-	var out, i, len, c;
-	out = "";
-	len = str.length;
-	for (i = 0; i < len; i++) {
-		c = str.charCodeAt(i);
-		if ((c >= 0x0001) && (c <= 0x007F)) {
-			out += str.charAt(i);
-		} else if (c > 0x07FF) {
-			out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-			out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
-			out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-		} else {
-			out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
-			out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-		}
-	}
-	return out;
-}
-
 function qrcode(options) {
 	// if options is string,
 	if (typeof(options) === "string") {
 		options	= { text: options };
 	}
+	
 	// support chinese chars
-	options.text = utf16to8(options.text);
+	var string = new tarsier.String(options.text);
+	options.text = string.convertTo("utf-8");
 	
 	var target = options.target || "#qrcode";
 	$(target).qrcode(options);
